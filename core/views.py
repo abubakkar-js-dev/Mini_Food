2ae85ab7864a01fe4cf3b65fc527675d10bd6ffb
abubakkar-js-dev <mdabubakkars182@gmail.com>
2026-02-25 07:22:02 +0600

    add: menu section designed

--- core/views.py
+++ core/views.py
@@ -3,4 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(req):
-    return render(req,'core/home.html')
+    menuItems = [
+        {'name': 'Classic Burger', 'price': '$12.99', 'rating': 4.9, 'image': 'heroImg', 'tag': 'Bestseller'},
+        {'name': 'Pepperoni Pizza', 'price': '$14.99', 'rating': 4.8, 'image': 'pizzaImg', 'tag': 'Popular'},
+        {'name': 'Salmon Poke Bowl', 'price': '$16.99', 'rating': 4.7, 'image': 'bowlImg', 'tag': None},
+        {'name': 'Crispy Chicken', 'price': '$11.99', 'rating': 4.6, 'image': 'chickenImg', 'tag': 'New'},
+        {'name': 'Garden Salad', 'price': '$9.99', 'rating': 4.5, 'image': 'saladImg', 'tag': None},
+        {'name': 'Lava Cake', 'price': '$8.99', 'rating': 4.9, 'image': 'dessertImg', 'tag': 'Must Try'},
+    ]
+    return render(req, 'core/home.html',context={'menuItems': menuItems})
